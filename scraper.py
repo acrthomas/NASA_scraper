2b965e9b68d9b2c75d9da522245de1ad55f4d22a
AC Thomas <saboteadores@gmail.com>
2018-10-12 22:56:33 -0500

    scraper.py
    a simple scraper using urllib and BeautifulSoup4

---
+++ scraper.py
@@ -0,0 +1,22 @@
+import os
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.parse import urljoin
+# From the APOD archive, follow each link, find the image and save it
+
+# URLLIB
+base_url = "http://apod.nasa.gov/apod/archivepix.html"
+download_directory = "apod_pix"
+content = urllib.request.urlopen(base_url).read()
+
+# BEAUTIFULSOUP
+for link in BeautifulSoup(content, "lxml").findAll("a"):
+    print("Following link:", link)
+    href = urljoin(base_url, link["href"])
+
+    content = urllib.request.urlopen(href).read()
+    for img in BeautifulSoup(content, "lxml").findAll("img"):
+        img_href = urljoin(href, img["src"])
+        print("Downloading image:", img_href)
+        img_name = img_href.split("/")[-1]
+        urllib.request.urlretrieve(img_href, os.path.join(download_directory, img_name))
